c7cf8dd6033f0eba9324a67b8c913d4b9dce8207
John McIntosh II <jmci10@hotmail.com>
2016-02-09 11:49:02 +0000

    Go's collection functions

---
+++ collection-functions.go
@@ -0,0 +1,87 @@
+// Go does not support generics
+// common to provide collection functions if and when 
+// needed
+
+package main
+
+import "strings"
+import "fmt"
+
+// returns 1st index of target string t, or -1 if no match
+func Index(vs []string, t string) int {
+  for i, v := range vs {
+    if v == t {
+      return i
+    }
+  }
+  return -1
+}
+
+// returns true if target string is in slice
+func Include(vs []string, t string) bool {
+  return Index(vs, t) >= 0
+}
+
+// returns true if one of strings in slice satisfies predicate f
+func Any(vs []string, f func(string) bool) bool {
+  for _, v := range vs {
+    if f(v) {
+      return true
+    }
+  }
+  return false
+}
+// returns true if all strings in slice satisfy predicate f
+func All(vs []string, f func(string) bool) bool {
+  for _, v := range vs {
+    if !f(v) {
+      return false
+    }
+  }
+  return true
+}
+// returns a new slice containing all string in slice that
+// satisfy predicate f
+func Filter(vs []string, f func(string) bool) []string {
+  vsf := make([]string, 0)
+  for _, v := range vs {
+    if f(v) {
+      vsf = append(vsf, v)
+    }
+  }
+  return vsf
+}
+
+
+// returns a new slice containing results of applying
+// function f to each string in original slice
+func Map(vs []string, f func(string) string) []string {
+  vsm := make([]string, len(vs))
+  for i, v := range vs {
+    vsm[i] = f(v)
+  }
+  return vsm
+}
+
+//using anonymous functions to try collection functions
+func main() {
+  var strs = []string{"peach", "apple", "pear", "plum"}
+  
+  fmt.Println(Index(strs, "pear"))
+  
+  fmt.Println(Include(strs, "grape"))
+  
+  fmt.Println(Any(strs, func(v string) bool {
+    return strings.HasPrefix(v, "p")
+  }))
+  
+  fmt.Println(All(strs, func(v string) bool {
+    return strings.HasPrefix(v, "p")
+  }))
+  
+  fmt.Println(Filter(strs, func(v string) bool {
+    return strings.Contains(v, "e")
+  }))
+  
+  fmt.Println(Map(strs, strings.ToUpper))
+}
