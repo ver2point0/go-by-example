33ee55272154604b35ae918716bfa657270d7040
John McIntosh II <jmci10@hotmail.com>
2016-02-25 05:27:18 +0000

    Go's base64 encoding

---
+++ base64-encoding.go
@@ -0,0 +1,24 @@
+// Go provides built-in support for base64
+
+package main
+
+import b64 "encoding/base64"
+import "fmt"
+
+func main() {
+  
+  // string to code encode/decode
+  data := "abc123!?$*&()'-=@~"
+  
+  sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+  fmt.Println(sEnc)
+  
+  sDec, _ := b64.StdEncoding.DecodeString(sEnc)
+  fmt.Println(string(sDec))
+  fmt.Println()
+  
+  uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+  fmt.Println(uEnc)
+  uDec, _ := b64.URLEncoding.DecodeString(uEnc)
+  fmt.Println(string(uDec))
+}
